b2d253b6ae861634be70407b89ae107ac4cfccbb
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-12-14 18:07:12 +0300

    Initial commit

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        /// <summary>
+        /// Код, выполняемый в первую очередь
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");//Вывод текста в консоль
+
+            Console.ReadKey();//Запрос нажатия клавииши
+        }
+
+    }
+}
---
+++ WriteWriteLineConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteWriteLineConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Код, выполняемый в первую очередь
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, ");//Вывод текста в консоль в строку
+            Console.Write("World");//Вывод текста в консоль в строку
+            Console.Write("!!!");//Вывод текста в консоль в строку
+            Console.ReadLine();//Задержка
+
+        }
+    }
+}
